1afbc63fd0855425668451f2104a0078aab9f844
CodeMyst <code@myst.rs>
2021-12-09 19:40:03 +0100

    aoc-09-02

---
+++ 09/aoc-09-02.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char INPUT_FILE[] = "input";
+
+int width = 0;
+int height = 0;
+
+// recursive flood fill
+int get_basin(int map[width][height], int x, int y) {
+    // out of bounds
+    if (x < 0 || y < 0 || x >= width || y >= height) return 0;
+
+    // highest point
+    if (map[x][y] == 9) return 0;
+
+    // already seen
+    if (map[x][y] == -1) return 0;
+
+    map[x][y] = -1;
+
+    int size = 1;
+
+    size += get_basin(map, x+1, y);
+    size += get_basin(map, x-1, y);
+    size += get_basin(map, x, y+1);
+    size += get_basin(map, x, y-1);
+
+    return size;
+}
+
+void print_map(int map[width][height]) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            printf("%02d ", map[x][y]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    FILE* fp = fopen(INPUT_FILE, "r");
+
+    if (fp == NULL) return 1;
+
+    // calculate the width and height
+    char buffer[200];
+    while (fgets(buffer, 200, fp) != NULL) {
+        width = strlen(buffer) - 1;
+
+        height++;
+    }
+    rewind(fp);
+
+    int map[width][height];
+
+    // load the map
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            char c = fgetc(fp);
+            map[x][y] = atoi(&c);
+        }
+        fgetc(fp);
+    }
+
+    int top[3] = {0};
+
+    // check lowest points
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            // number of neighbours larger then current cell
+            int n = 0;
+
+            int c = map[x][y];
+
+            // if on edge
+            if (x == 0) n++;
+            if (x == width - 1) n++;
+            if (y == 0) n++;
+            if (y == height - 1) n++;
+
+            // if not on edge and larger
+            if (x > 0 && map[x-1][y] > c) n++;
+            if (x < width - 1 && map[x+1][y] > c) n++;
+            if (y > 0 && map[x][y-1] > c) n++;
+            if (y < height - 1 && map[x][y+1] > c) n++;
+
+            // if lowest point
+            if (n == 4) {
+                // copy map into new array so visited cells can be marked
+                int nmap[width][height];
+                memcpy(nmap, map, width * height * sizeof(int));
+                int size = get_basin(nmap, x, y);
+
+                // check top 3 basins
+                if (size > top[0]) {
+                    top[2] = top[1];
+                    top[1] = top[0];
+                    top[0] = size;
+                } else if (size > top[1]) {
+                    top[2] = top[1];
+                    top[1] = size;
+                } else if (size > top[2]) {
+                    top[2] = size;
+                }
+            }
+        }
+    }
+
+    printf("res: %d\n", top[0] * top[1] * top[2]);
+
+    return 0;
+}
